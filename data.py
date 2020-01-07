5a1f8540f9d27e7e8b51bc5789dd0fa5c7492804
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-07 15:32:54 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,159 @@
+import torch
+import difflib#, librosa
+import wave
+import numpy as np
+import scipy
+import json
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+sample_rate = 16000
+window_size = 0.02
+window_stride = 0.01
+n_fft = int(sample_rate * window_size)
+win_length = n_fft
+hop_length = int(sample_rate * window_stride)
+window = "hamming"
+
+
+def load_audio(wav_path, normalize=True):  # -> numpy array
+	if ".wav" in wav_path:
+		with wave.open(wav_path) as wav:
+			wav = np.frombuffer(wav.readframes(wav.getnframes()), dtype="int16")
+			wav = wav.astype("float")
+	elif ".bin" in wav_path:
+		wav = np.fromfile(wav_path, dtype="int16").astype("float")
+	else:
+		print("Error: ", wav_path)
+		
+	if normalize:
+		return (wav - wav.mean()) / wav.std()
+	else:
+		return wav
+
+
+def spectrogram(wav, normalize=True):
+# librosa
+	D = librosa.stft(
+		wav, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window
+	)
+
+	spec, phase = librosa.magphase(D)
+	spec = np.log1p(spec)
+	
+# handled
+	
+	spec = torch.FloatTensor(spec)
+
+	if normalize:
+		spec = (spec - spec.mean()) / spec.std()
+
+	return spec
+
+# 获取信号的时频图
+def compute_fbank(file, normalize=True):
+	# _path = file.replace(".bin", ".npz")
+	# try:
+		# if os.path.exists(_path):
+			# _data = np.load(_path)["data"]
+			# return _data
+	# except Exception as e:
+		# print("Error:", e)
+	x = np.linspace(0, 400 - 1, 400, dtype=np.int64)
+	w = 0.54 - 0.46 * np.cos(2 * np.pi * (x) / (400 - 1))  # 汉明窗
+	# fs, wavsignal = wav.read(file)
+	fs = 16000
+	wavsignal = np.fromfile(file, dtype=np.int16)
+	# wav波形 加时间窗以及时移10ms
+	time_window = 25  # 单位ms
+	wav_arr = wavsignal # np.array(wavsignal)
+	range0_end = int(len(wavsignal) / fs * 1000 - time_window) // 10 + 1 # 计算循环终止的位置，也就是最终生成的窗数
+	data_input = np.zeros((range0_end, 200), dtype=np.float)  # 用于存放最终的频率特征数据
+	data_line = np.zeros((1, 400), dtype=np.float)
+	for i in range(0, range0_end):
+		p_start = i * 160
+		p_end = p_start + 400
+		data_line = wav_arr[p_start:p_end]
+		data_line = data_line * w  # 加窗
+		data_line = np.abs(fft(data_line))
+		data_input[i] = data_line[0:200]  # 设置为400除以2的值（即200）是取一半数据，因为是对称的
+	data_input = np.log(data_input + 1)
+	
+	spec = torch.FloatTensor(data_input)
+
+	if normalize:
+		spec = (spec - spec.mean()) / spec.std()
+	
+	# data_input = data_input[::]
+	return spec
+	
+def GetEditDistance(str1, str2):
+	leven_cost = 0
+	s = difflib.SequenceMatcher(None, str1, str2)
+	for tag, i1, i2, j1, j2 in s.get_opcodes():
+		if tag == 'replace':
+			leven_cost += max(i2-i1, j2-j1)
+		elif tag == 'insert':
+			leven_cost += (j2-j1)
+		elif tag == 'delete':
+			leven_cost += (i2-i1)
+	return leven_cost
+
+class MASRDataset(Dataset):
+	def __init__(self, index_path, labels_path):
+		if ".json" not in index_path:
+			with open(index_path) as f:
+				idx = f.readlines()
+			idx = [x.strip().split(",", 1) for x in idx]
+		else:
+			with open(index_path, "r") as f:
+				idx = json.load(f)
+		self.idx = idx
+		with open(labels_path) as f:
+			labels = json.load(f)
+		self.labels = dict([(labels[i], i) for i in range(len(labels))])
+		self.labels_str = labels
+
+	def __getitem__(self, index):
+		wav_path, transcript = self.idx[index]
+		wav = load_audio(wav_path)
+		spect = spectrogram(wav)
+		transcript = list(filter(None, [self.labels.get(x) for x in transcript]))
+
+		return spect, transcript
+
+	def __len__(self):
+		return len(self.idx)
+
+
+def _collate_fn(batch):
+	def func(p):
+		return p[0].size(1)
+
+	batch = sorted(batch, key=lambda sample: sample[0].size(1), reverse=True)
+	longest_sample = max(batch, key=func)[0]
+	freq_size = longest_sample.size(0)
+	minibatch_size = len(batch)
+	max_seqlength = longest_sample.size(1)
+	inputs = torch.zeros(minibatch_size, freq_size, max_seqlength)
+	input_lens = torch.IntTensor(minibatch_size)
+	target_lens = torch.IntTensor(minibatch_size)
+	targets = []
+	for x in range(minibatch_size):
+		sample = batch[x]
+		tensor = sample[0]
+		target = sample[1]
+		seq_length = tensor.size(1)
+		inputs[x].narrow(1, 0, seq_length).copy_(tensor)
+		input_lens[x] = seq_length
+		target_lens[x] = len(target)
+		targets.extend(target)
+	targets = torch.IntTensor(targets)
+	return inputs, targets, input_lens, target_lens
+
+
+class MASRDataLoader(DataLoader):
+	def __init__(self, *args, **kwargs):
+		super(MASRDataLoader, self).__init__(*args, **kwargs)
+		self.collate_fn = _collate_fn
+
---
+++ train.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+import data
+from warpctc_pytorch import CTCLoss
+from models.conv import GatedConv
+from tqdm import tqdm
+from decoder import GreedyDecoder
+import tensorboardX as tensorboard
+import torch.nn.functional as F
+import os, json, random
+
+# 1. set random seed
+random.seed(2050)
+np.random.seed(2050)
+torch.manual_seed(2050)
+torch.cuda.manual_seed_all(2050)
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0, 3"
+torch.backends.cudnn.benchmark = True
+# warnings.filterwarnings('ignore')
+
+# MultiGpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if torch.cuda.device_count() > 1:
+	# config.batch_size = config.batch_size * torch.cuda.device_count()
+	print("Let's use", torch.cuda.device_count(), "GPUs - ", config.batch_size)
+	model = nn.DataParallel(model)#, device_ids=[0, 2]
+
+def train(
+    model,
+    epochs=1000,
+    batch_size=64,
+    train_index_path="dataset/train_index.json",
+    dev_index_path="dataset/dev_index.json",
+    labels_path="dataset/labels.json",
+    learning_rate=0.6,
+    momentum=0.8,
+    max_grad_norm=0.2,
+    weight_decay=0,
+):
+    train_dataset = data.MASRDataset(train_index_path, labels_path)
+    batchs = (len(train_dataset) + batch_size - 1) // batch_size
+    dev_dataset = data.MASRDataset(dev_index_path, labels_path)
+    train_dataloader = data.MASRDataLoader(
+        train_dataset, batch_size=batch_size, num_workers=8
+    )
+    train_dataloader_shuffle = data.MASRDataLoader(
+        train_dataset, batch_size=batch_size, num_workers=8, shuffle=True
+    )
+    dev_dataloader = data.MASRDataLoader(
+        dev_dataset, batch_size=batch_size, num_workers=8
+    )
+    parameters = model.parameters()
+    optimizer = torch.optim.SGD(
+        parameters,
+        lr=learning_rate,
+        momentum=momentum,
+        nesterov=True,
+        weight_decay=weight_decay,
+    )
+    ctcloss = CTCLoss(size_average=True)
+    # lr_sched = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.985)
+    writer = tensorboard.SummaryWriter()
+    gstep = 0
+    for epoch in range(epochs):
+        epoch_loss = 0
+        if epoch > 0:
+            train_dataloader = train_dataloader_shuffle
+        # lr_sched.step()
+        lr = get_lr(optimizer)
+        writer.add_scalar("lr/epoch", lr, epoch)
+        for i, (x, y, x_lens, y_lens) in enumerate(train_dataloader):
+            x = x.to(device)
+            out, out_lens = model(x, x_lens)
+            out = out.transpose(0, 1).transpose(0, 2)
+            loss = ctcloss(out, y, out_lens, y_lens)
+            optimizer.zero_grad()
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+            optimizer.step()
+            epoch_loss += loss.item()
+            writer.add_scalar("loss/step", loss.item(), gstep)
+            gstep += 1
+            print(
+                "[{}/{}][{}/{}]\tLoss = {}".format(
+                    epoch + 1, epochs, i, int(batchs), loss.item()
+                )
+            )
+        epoch_loss = epoch_loss / batchs
+        cer = eval(model, dev_dataloader)
+        writer.add_scalar("loss/epoch", epoch_loss, epoch)
+        writer.add_scalar("cer/epoch", cer, epoch)
+        print("Epoch {}: Loss= {}, CER = {}".format(epoch, epoch_loss, cer))
+        torch.save(model, "pretrained/model_{}_{}_{}.pth".format(epoch, epoch_loss, cer))
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+
+def eval(model, dataloader):
+    model.eval()
+    decoder = GreedyDecoder(dataloader.dataset.labels_str)
+    cer = 0
+    print("decoding")
+    with torch.no_grad():
+        for i, (x, y, x_lens, y_lens) in tqdm(enumerate(dataloader)):
+            x = x.to(device)
+            outs, out_lens = model(x, x_lens)
+            outs = F.softmax(outs, 1)
+            outs = outs.transpose(1, 2)
+            ys = []
+            offset = 0
+            for y_len in y_lens:
+                ys.append(y[offset : offset + y_len])
+                offset += y_len
+            out_strings, out_offsets = decoder.decode(outs, out_lens)
+            y_strings = decoder.convert_to_strings(ys)
+            for pred, truth in zip(out_strings, y_strings):
+                trans, ref = pred[0], truth[0]
+                cer += decoder.cer(trans, ref) / float(len(ref))
+        cer /= len(dataloader.dataset)
+    model.train()
+    return cer
+
+
+if __name__ == "__main__":
+    with open("dataset/labels.json") as f:
+        vocabulary = json.load(f)
+        vocabulary = "".join(vocabulary)
+    model = GatedConv(vocabulary)
+    model.to(device)
+    train(model)
