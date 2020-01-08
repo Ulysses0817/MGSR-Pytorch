e9cea578f17ba795c6832e9fee71023b3173fbe5
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-08 10:14:31 +0800

    add argpaser support

--- train.py
+++ train.py
@@ -28,108 +28,132 @@ if torch.cuda.device_count() > 1:
 	model = nn.DataParallel(model)#, device_ids=[0, 2]
 
 def train(
-    model,
-    epochs=1000,
-    batch_size=64,
-    train_index_path="dataset/train_index.json",
-    dev_index_path="dataset/dev_index.json",
-    labels_path="dataset/labels.json",
-    learning_rate=0.6,
-    momentum=0.8,
-    max_grad_norm=0.2,
-    weight_decay=0,
+	model,
+	epochs=1000,
+	batch_size=64,
+	train_index_path="./dataset/train_index.json",
+	dev_index_path="./dataset/dev_index.json",
+	labels_path="./dataset/labels.json",
+	learning_rate=0.6,
+	momentum=0.8,
+	max_grad_norm=0.2,
+	weight_decay=0,
 ):
-    train_dataset = data.MASRDataset(train_index_path, labels_path)
-    batchs = (len(train_dataset) + batch_size - 1) // batch_size
-    dev_dataset = data.MASRDataset(dev_index_path, labels_path)
-    train_dataloader = data.MASRDataLoader(
-        train_dataset, batch_size=batch_size, num_workers=8
-    )
-    train_dataloader_shuffle = data.MASRDataLoader(
-        train_dataset, batch_size=batch_size, num_workers=8, shuffle=True
-    )
-    dev_dataloader = data.MASRDataLoader(
-        dev_dataset, batch_size=batch_size, num_workers=8
-    )
-    parameters = model.parameters()
-    optimizer = torch.optim.SGD(
-        parameters,
-        lr=learning_rate,
-        momentum=momentum,
-        nesterov=True,
-        weight_decay=weight_decay,
-    )
-    ctcloss = CTCLoss(size_average=True)
-    # lr_sched = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.985)
-    writer = tensorboard.SummaryWriter()
-    gstep = 0
-    for epoch in range(epochs):
-        epoch_loss = 0
-        if epoch > 0:
-            train_dataloader = train_dataloader_shuffle
-        # lr_sched.step()
-        lr = get_lr(optimizer)
-        writer.add_scalar("lr/epoch", lr, epoch)
-        for i, (x, y, x_lens, y_lens) in enumerate(train_dataloader):
-            x = x.to(device)
-            out, out_lens = model(x, x_lens)
-            out = out.transpose(0, 1).transpose(0, 2)
-            loss = ctcloss(out, y, out_lens, y_lens)
-            optimizer.zero_grad()
-            loss.backward()
-            nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
-            optimizer.step()
-            epoch_loss += loss.item()
-            writer.add_scalar("loss/step", loss.item(), gstep)
-            gstep += 1
-            print(
-                "[{}/{}][{}/{}]\tLoss = {}".format(
-                    epoch + 1, epochs, i, int(batchs), loss.item()
-                )
-            )
-        epoch_loss = epoch_loss / batchs
-        cer = eval(model, dev_dataloader)
-        writer.add_scalar("loss/epoch", epoch_loss, epoch)
-        writer.add_scalar("cer/epoch", cer, epoch)
-        print("Epoch {}: Loss= {}, CER = {}".format(epoch, epoch_loss, cer))
-        torch.save(model, "pretrained/model_{}_{}_{}.pth".format(epoch, epoch_loss, cer))
+	train_dataset = data.MASRDataset(train_index_path, labels_path)
+	batchs = (len(train_dataset) + batch_size - 1) // batch_size
+	dev_dataset = data.MASRDataset(dev_index_path, labels_path)
+	train_dataloader = data.MASRDataLoader(
+		train_dataset, batch_size=batch_size, num_workers=8
+	)
+	train_dataloader_shuffle = data.MASRDataLoader(
+		train_dataset, batch_size=batch_size, num_workers=8, shuffle=True
+	)
+	dev_dataloader = data.MASRDataLoader(
+		dev_dataset, batch_size=batch_size, num_workers=8
+	)
+	parameters = model.parameters()
+	optimizer = torch.optim.SGD(
+		parameters,
+		lr=learning_rate,
+		momentum=momentum,
+		nesterov=True,
+		weight_decay=weight_decay,
+	)
+	ctcloss = CTCLoss(size_average=True)
+	# lr_sched = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.985)
+	writer = tensorboard.SummaryWriter()
+	gstep = 0
+	for epoch in range(epochs):
+		epoch_loss = 0
+		if epoch > 0:
+			train_dataloader = train_dataloader_shuffle
+		# lr_sched.step()
+		lr = get_lr(optimizer)
+		writer.add_scalar("lr/epoch", lr, epoch)
+		for i, (x, y, x_lens, y_lens) in enumerate(train_dataloader):
+			x = x.to(device)
+			out, out_lens = model(x, x_lens)
+			out = out.transpose(0, 1).transpose(0, 2)
+			loss = ctcloss(out, y, out_lens, y_lens)
+			optimizer.zero_grad()
+			loss.backward()
+			nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+			optimizer.step()
+			epoch_loss += loss.item()
+			writer.add_scalar("loss/step", loss.item(), gstep)
+			gstep += 1
+			print(
+				"[{}/{}][{}/{}]\tLoss = {}".format(
+					epoch + 1, epochs, i, int(batchs), loss.item()
+				)
+			)
+		epoch_loss = epoch_loss / batchs
+		cer = eval(model, dev_dataloader)
+		writer.add_scalar("loss/epoch", epoch_loss, epoch)
+		writer.add_scalar("cer/epoch", cer, epoch)
+		print("Epoch {}: Loss= {}, CER = {}".format(epoch, epoch_loss, cer))
+		torch.save(model, "pretrained/model_{}_{}_{}.pth".format(epoch, epoch_loss, cer))
 
 
 def get_lr(optimizer):
-    for param_group in optimizer.param_groups:
-        return param_group["lr"]
+	for param_group in optimizer.param_groups:
+		return param_group["lr"]
 
 
 def eval(model, dataloader):
-    model.eval()
-    decoder = GreedyDecoder(dataloader.dataset.labels_str)
-    cer = 0
-    print("decoding")
-    with torch.no_grad():
-        for i, (x, y, x_lens, y_lens) in tqdm(enumerate(dataloader)):
-            x = x.to(device)
-            outs, out_lens = model(x, x_lens)
-            outs = F.softmax(outs, 1)
-            outs = outs.transpose(1, 2)
-            ys = []
-            offset = 0
-            for y_len in y_lens:
-                ys.append(y[offset : offset + y_len])
-                offset += y_len
-            out_strings, out_offsets = decoder.decode(outs, out_lens)
-            y_strings = decoder.convert_to_strings(ys)
-            for pred, truth in zip(out_strings, y_strings):
-                trans, ref = pred[0], truth[0]
-                cer += decoder.cer(trans, ref) / float(len(ref))
-        cer /= len(dataloader.dataset)
-    model.train()
-    return cer
+	model.eval()
+	decoder = GreedyDecoder(dataloader.dataset.labels_str)
+	cer = 0
+	print("decoding")
+	with torch.no_grad():
+		for i, (x, y, x_lens, y_lens) in tqdm(enumerate(dataloader)):
+			x = x.to(device)
+			outs, out_lens = model(x, x_lens)
+			outs = F.softmax(outs, 1)
+			outs = outs.transpose(1, 2)
+			ys = []
+			offset = 0
+			for y_len in y_lens:
+				ys.append(y[offset : offset + y_len])
+				offset += y_len
+			out_strings, out_offsets = decoder.decode(outs, out_lens)
+			y_strings = decoder.convert_to_strings(ys)
+			for pred, truth in zip(out_strings, y_strings):
+				trans, ref = pred[0], truth[0]
+				cer += decoder.cer(trans, ref) / float(len(ref))
+		cer /= len(dataloader.dataset)
+	model.train()
+	return cer
 
 
 if __name__ == "__main__":
-    with open("dataset/labels.json") as f:
-        vocabulary = json.load(f)
-        vocabulary = "".join(vocabulary)
-    model = GatedConv(vocabulary)
-    model.to(device)
-    train(model)
+	import argparse
+	parser = argparse.ArgumentParser()
+	# parser.description='喂我两个数字，我就吐出他们的积'
+	parser.add_argument("-e","--epochs", help="迭代次数",type=int, default=200)
+	parser.add_argument("-b","--batch_size", type=int, default=32)
+	parser.add_argument("-tr","--train_index_path", default="./dataset/train_index.json")
+	parser.add_argument("-dev","--dev_index_path", default="./dataset/dev_index.json")
+	parser.add_argument("-l","--labels_path", default="./dataset/labels.json")
+	parser.add_argument("-lr","--learning_rate", default=0.6, type=float)
+	parser.add_argument("-mgn","--max_grad_norm", default=0.2, type=float)
+	parser.add_argument("-mon","--momentum", default=0.8, type=float)
+	parser.add_argument("-w","--weight_decay", default=0, type=float)	
+	
+	args = parser.parse_args()
+	
+	with open("./dataset/labels.json") as f:
+		vocabulary = json.load(f)
+		vocabulary = "".join(vocabulary)
+	model = GatedConv(vocabulary)
+	model.to(device)
+	train(model, 	
+		epochs=args.epochs,
+		batch_size=args.batch_size,
+		train_index_path=args.train_index_path,
+		dev_index_path=args.dev_index_path,
+		labels_path=args.labels_path,
+		learning_rate=args.learning_rate,
+		momentum=args.momentum,
+		max_grad_norm=args.max_grad_norm,
+		weight_decay=args.weight_decay)
