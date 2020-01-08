565054d98416cc4e76de6c90ab7afd6a1b8bd37e
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-09 01:37:40 +0800

    Add files via upload

--- test_predict.py
+++ test_predict.py
@@ -1,12 +1,26 @@
 import sys
 import swifter
 import pandas as pd
-from beamdecode import predict
+from beamdecode import model_setup, predict
 
-test_orig = pd.read_csv("/kaggle/input/speechrecon/test.csv")
+def getres(wp, m, d):
+	res = predict(wp, m, d)
+#     preds.append(res)
+	with open("./Recoder_new.txt", "a+") as fw:
+		fw.write(res+"\n")
+	return res
+model, decoder = model_setup(sys.argv[2])#"./pretrained/model_6_33.6234_0.6837.pth"
+
+test_orig = pd.read_csv("./dataset/test.csv")
 test = test_orig.loc[test_orig.words==" "]
 test["wav_path"] = test["Unnamed: 0"].swifter.apply(lambda x: "/kaggle/input/magicdata/test/test_byte(%s)/"%sys.argv[1]+"wave_%s.bin"%x)
-test_orig.to_csv("./test_orig.csv", index = False)
-test["pred"] = test.wav_path.swifter.apply(lambda x: predict(x))
+test.to_csv("./test_orig.csv", index = False)
+# test["pred"] = test.wav_path.swifter.apply(lambda x: predict(x))
+if os.path.exists("./Recoder_new.txt"):
+	os.remove("./Recoder_new.txt")
+ress = []
+for i in test["wav_path"].values:
+	ress.append(getres(i, model, decoder))
+test["pred"] = ress
 test.to_csv("./test_res.csv", index = False)
-test_orig.to_csv("./test_orig_res.csv", index = False)
+# test_orig.to_csv("./test_orig_res.csv", index = False)
