7082fb230a417b4b1f5d2d7a98f2915445715f54
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-09 22:36:17 +0800

    Add files via upload

--- single_predict.py
+++ single_predict.py
@@ -5,8 +5,11 @@ def getres(wp, m, d):
 	res = predict(wp, m, d)
 #     preds.append(res)
 	with open("./Recoder_new.txt", "a+") as fw:
-		fw.write(",".join([wp, res+"\n"]))
+		fw.write(",".join([wp, res+"\n\r"]))
 	return res
 	
-model, decoder = model_setup("./pretrained/model_6_33.6234_0.6837.pth")#sys.argv[1]
-getres(sys.argv[1], model, decoder)
+model, decoder = model_setup(sys.argv[2])#"./pretrained/model_6_33.6234_0.6837.pth"
+with open(sys.argv[1], "r") as fr:
+	paths = fr.readlines()
+for p in paths:
+	getres(p.strip(), model, decoder)
