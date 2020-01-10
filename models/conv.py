af6645c9e12219877fc432d62f17b8fae99f0e43
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-10 17:55:15 +0800

    Add files via upload

--- models/conv.py
+++ models/conv.py
@@ -25,14 +25,13 @@ class GatedConv(MASRModel):
 	""" This is a model between Wav2letter and Gated Convnets.
 		The core block of this model is Gated Convolutional Network"""
 
-	def __init__(self, vocabulary, blank=0, name="masr", pretrained=None):
+	def __init__(self, vocabulary, blank=0, name="masr"):
 		""" vocabulary : str : string of all labels such that vocaulary[0] == ctc_blank  """
 		super().__init__(vocabulary=vocabulary, name=name, blank=blank)
 		self.blank = blank
 		self.vocabulary = vocabulary
 		self.name = name
-		self.pretrained_path = pretrained
-		output_units = len(vocabulary)
+		self.output_units = len(vocabulary)
 		modules = []
 		modules.append(ConvBlock(nn.Conv1d(161, 500, 48, 2, 97), 0.2))
 
@@ -41,12 +40,9 @@ class GatedConv(MASRModel):
 
 		modules.append(ConvBlock(nn.Conv1d(250, 2000, 32, 1), 0.5))
 		modules.append(ConvBlock(nn.Conv1d(1000, 2000, 1, 1), 0.5))
-		modules.append(weight_norm(nn.Conv1d(1000, output_units, 1, 1)))
+		modules.append(weight_norm(nn.Conv1d(1000, self.output_units, 1, 1)))
 
 		self.cnn = nn.Sequential(*modules)
-		if self.pretrained_path is not None:
-			print("loading pretrained model: ", self.pretrained_path)
-			self.load_pretrained(output_units)
 
 	def forward(self, x, lens):  # -> B * V * T
 		x = self.cnn(x)
@@ -68,18 +64,3 @@ class GatedConv(MASRModel):
 		text = self.decode(out, out_len)
 		self.train()
 		return text[0]
-
-	def load_pretrained(self, model_output_units):
-		package = torch.load(self.pretrained_path, map_location="cpu")
-		try:
-			pretrained_dict = package["state_dict"]
-			package_output_units = len(package["config"]["vocabulary"])
-		except:
-			pretrained_dict = package.state_dict()
-			package_output_units = len(package.vocabulary)
-		if package_output_units != model_output_units:
-			pretrained_dict = {k: v for k, v in pretrained_dict.items() if "cnn.10" not in k}
-			print(pretrained_dict.keys())
-		model_dict = self.cnn.state_dict()
-		model_dict.update(pretrained_dict)
-		self.cnn.load_state_dict(model_dict)
