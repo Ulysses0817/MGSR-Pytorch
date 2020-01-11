457da687d841695c33e590fe14c1557b90e58e67
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-11 15:45:27 +0800

    Add files via upload

--- beamdecode.py
+++ beamdecode.py
@@ -13,11 +13,15 @@ def model_setup(pretrained_path = "pretrained/gated-conv.pth",
 				cutoff_prob = 1.0,
 				beam_width = 32,
 				num_processes = 4,
-				blank_index = 0):
+				blank_index = 0,
+				vocabulary = None):
 	
 	model = GatedConv.load(pretrained_path)
 	model.eval()
-
+	
+	if vocabulary is not None:
+		model.vocabulary = vocabulary
+	
 	decoder = CTCBeamDecoder(
 		model.vocabulary,
 		lm_path,
@@ -32,18 +36,18 @@ def model_setup(pretrained_path = "pretrained/gated-conv.pth",
 	return model, decoder
 
 def translate(vocab, out, out_len):
-    return "".join([vocab[x] for x in out[0:out_len]])
+	return "".join([vocab[x] for x in out[0:out_len]])
 
 
 def predict(f, model, decoder):
-    wav = feature.load_audio(f)
-    spec = feature.spectrogram(wav)
-    spec.unsqueeze_(0)
-    with torch.no_grad():
-        y = model.cnn(spec)
-        y = F.softmax(y, 1)
-    y_len = torch.tensor([y.size(-1)])
-    y = y.permute(0, 2, 1)  # B * T * V
-    # print("decoding")
-    out, score, offset, out_len = decoder.decode(y, y_len)
-    return translate(model.vocabulary, out[0][0], out_len[0][0])
+	wav = feature.load_audio(f)
+	spec = feature.spectrogram(wav)
+	spec.unsqueeze_(0)
+	with torch.no_grad():
+		y = model.cnn(spec)
+		y = F.softmax(y, 1)
+	y_len = torch.tensor([y.size(-1)])
+	y = y.permute(0, 2, 1)  # B * T * V
+	# print("decoding")
+	out, score, offset, out_len = decoder.decode(y, y_len)
+	return translate(model.vocabulary, out[0][0], out_len[0][0])
--- single_predict.py
+++ single_predict.py
@@ -8,7 +8,24 @@ def getres(wp, m, d):
 		fw.write(",".join([wp, res+"\n\r"]))
 	return res
 	
-model, decoder = model_setup(sys.argv[2])#"./pretrained/model_6_33.6234_0.6837.pth"
+vocab = model.vocabulary
+
+gt1 = []
+for i in vocab:
+    if len(i) >1:
+        gt1.append(i)
+import codecs
+start,end = (0x4E00, 0x9FA5)
+hanzis = []
+for codepoint in range(int(start),int(end)):
+    if chr(codepoint) not in vocab:
+        hanzis.append(chr(codepoint))
+hanzis = hanzis[:len(gt1)]
+en2chr = dict(zip(gt1, hanzis))
+vocab = [en2chr[i] if i in gt1 else i for i in vocab]
+
+model, decoder = model_setup(sys.argv[2], vocabulary = vocab)#"./pretrained/model_6_33.6234_0.6837.pth"
+print(model, decoder)
 with open(sys.argv[1], "r") as fr:
 	paths = fr.readlines()
 for p in paths:
