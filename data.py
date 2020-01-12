ce1f2157eeff4e3d741ac506eba06e11657bbeca
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-12 23:00:19 +0800

    Add files via upload

--- data.py
+++ data.py
@@ -160,7 +160,10 @@ class MASRDataLoader(DataLoader):
 			inputs[x].narrow(1, 0, seq_length).copy_(tensor)
 			input_lens[x] = seq_length
 			target_lens[x] = len(target)
-			targets.append(target)
+			if self.dataset.mode == "test": 
+				targets.extend(target)
+			else:
+				targets.append(target)
 		if self.dataset.mode == "test":
 			return inputs, targets, input_lens
 		else:
