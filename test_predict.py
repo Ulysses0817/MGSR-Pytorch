44cd29bf17054466b016013f0a2c4bb5f040fd89
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-13 22:12:26 +0800

    Add files via upload

--- test_predict.py
+++ test_predict.py
@@ -41,7 +41,38 @@ def test(model, dataloader, device):
 	# except Exception as e:
 		# print(e)
 	return results, targets
-	
+
+def eval(model, dataloader,device):
+	model.eval()
+	decoder = GreedyDecoder(dataloader.dataset.labels_str)
+	from warpctc_pytorch import CTCLoss
+	ctcloss = CTCLoss(size_average=True)
+	cer = 0
+	epoch_loss = 0
+	print("decoding")
+	with torch.no_grad():
+		for i, (x, y, x_lens, y_lens) in tqdm(enumerate(dataloader)):
+			x = x.to(device)
+			outs, out_lens = model(x, x_lens)
+			loss = ctcloss(outs.transpose(0, 1).transpose(0, 2), y, out_lens, y_lens)
+			epoch_loss += loss.item()
+			outs = F.softmax(outs, 1)
+			outs = outs.transpose(1, 2)
+			ys = []
+			offset = 0
+			for y_len in y_lens:
+				ys.append(y[offset : offset + y_len])
+				offset += y_len
+			out_strings, out_offsets = decoder.decode(outs, out_lens)
+			y_strings = decoder.convert_to_strings(ys)
+			for pred, truth in zip(out_strings, y_strings):
+				trans, ref = pred[0], truth[0]
+				cer += decoder.cer(trans, ref) / float(len(ref))
+		cer /= len(dataloader.dataset)
+		epoch_loss /= i+1
+	print("cer:{}, epoch_loss:{}".format(cer, epoch_loss))
+	return cer, epoch_loss
+
 if __name__ == "__main__":
 	import argparse, ast
 	parser = argparse.ArgumentParser()
@@ -51,6 +82,7 @@ if __name__ == "__main__":
 	parser.add_argument("-te","--test_index_path", default="./dataset/test.json")
 	parser.add_argument("-l","--labels_path", default="./dataset/labels.json")
 	parser.add_argument("-cpu","--num_workers", default=8, type=int)
+	parser.add_argument("-m","--mode", default="test")
 	
 	args = parser.parse_args()
 	# MultiGpu
@@ -63,6 +95,9 @@ if __name__ == "__main__":
 	model, dataloader = model_setup(args)
 	model.to(device)
 	
-	results, targets = test(model, dataloader, device)
+	if args.mode == "test":
+		results, targets = test(model, dataloader, device)
+	else:
+		results, targets = eval(model, dataloader, device)
 	with open("./greedy_results.json", "w") as fw:
 		json.dump([results, targets], fw)
