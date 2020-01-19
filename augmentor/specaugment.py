c9e5c44a89a36c02f98941297451173e153044b4
Ulysses0817 <29434950+Ulysses0817@users.noreply.github.com>
2020-01-19 22:10:16 +0800

    Add files via upload

--- augmentor/specaugment.py
+++ augmentor/specaugment.py
@@ -281,7 +281,7 @@ def interpolate_bilinear(grid,
     query_type = query_points.dtype
     grid_type = grid.dtype
     grid_device = grid.device
-
+    assert(grid_type==query_type)
     num_queries = query_points.shape[1]
 
     alphas = []
@@ -308,8 +308,8 @@ def interpolate_bilinear(grid,
 
         # alpha has the same type as the grid, as we will directly use alpha
         # when taking linear combinations of pixel values from the image.
-
-        alpha = torch.tensor((queries - floor), dtype=grid_type, device=grid_device)
+        
+        alpha = (queries - floor).clone().detach()#torch.tensor((queries - floor), dtype=grid_type, device=grid_device)
         min_alpha = torch.tensor(0.0, dtype=grid_type, device=grid_device)
         max_alpha = torch.tensor(1.0, dtype=grid_type, device=grid_device)
         alpha = torch.min(torch.max(min_alpha, alpha), max_alpha)
